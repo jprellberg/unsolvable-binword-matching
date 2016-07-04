d810382c44890df0c5f5f25827d0943ab06e0b9b
Jonas Prellberg <jonas.prellberg@uni-oldenburg.de>
2016-07-04 18:51:03 +0200

    Improved Harro's LetterCounting code.

--- src/main/java/uniol/matcher/LetterCountingUBM.java
+++ src/main/java/uniol/matcher/LetterCountingUBM.java
@@ -31,19 +31,16 @@ public class LetterCountingUBM implements UnsolvableBinwordMatcher {
 	 * separation points.
 	 */
 	public static class Interval {
-		int minNum;
-		int minDen;
-		int maxNum;
-		int maxDen;
+		private final int minNum;
+		private final int minDen;
+		private final int maxNum;
+		private final int maxDen;
 
-		public Interval() {
-		}
-
-		public void set(int num1, int den1, int num2, int den2) {
-			this.minNum = num1;
-			this.minDen = den1;
-			this.maxNum = num2;
-			this.maxDen = den2;
+		public Interval(int minNum, int minDen, int maxNum, int maxDen) {
+			this.minNum = minNum;
+			this.minDen = minDen;
+			this.maxNum = maxNum;
+			this.maxDen = maxDen;
 		}
 
 		public boolean isInfinite() {
@@ -59,8 +56,9 @@ public class LetterCountingUBM implements UnsolvableBinwordMatcher {
 		}
 
 		public float rightQuot() {
-			if (maxDen == 0)
+			if (maxDen == 0) {
 				return 0;
+			}
 			return ((float) maxNum / (float) maxDen);
 		}
 
@@ -77,52 +75,41 @@ public class LetterCountingUBM implements UnsolvableBinwordMatcher {
 		}
 
 		public int getMaxDen() {
-			if (maxDen == 0 && maxNum == 0)
-				return 1; // replace 0/0 by 0/1
+			if (maxDen == 0 && maxNum == 0) {
+				// replace 0/0 by 0/1
+				return 1;
+			}
 			return maxDen;
 		}
 	}
 
 	@Override
 	public boolean isUnsolvableBinaryWord(String word) {
-		// word length
-		int len = word.length();
-		// position of the separation point in our word (n = before
-		// position n)
-		int seppt;
-
 		// split off the last letter ("the letter to append")
-		char inLetter = word.charAt(len - 1);
-		--len;
+		final char inLetter = word.charAt(word.length() - 1);
+		// remaining word length
+		final int len = word.length() - 1;
 
 		// create space to save the intervals in which the arc weights
 		// have to reside
 		Interval[] intervals = new Interval[len + 1];
-		for (int i = 0; i < len + 1; ++i) {
-			intervals[i] = new Interval();
-		}
 
-		// letter at the separation point
-		char sepLetter = word.charAt(0);
-
-		// temporary (de)nominators
-		int tmpMinNum, tmpMinDen, tmpMaxNum, tmpMaxDen;
-		// number of a's/b's in prefix or postfix at the separation
-		// point
-		int numa, numb;
-
-		// for all possible separation points
-		for (seppt = 1; seppt < len; ++seppt) {
+		// for all possible separation points seppt in our word
+		// (n = before position n)
+		for (int seppt = 1; seppt < len; ++seppt) {
 			// letter allowed at the separation point
-			sepLetter = word.charAt(seppt);
+			char sepLetter = word.charAt(seppt);
+
+			// number of a's/b's in prefix or postfix at the
+			// separation point
+			int numa = 0;
+			int numb = 0;
+			int tmpMinNum = 0;
+			int tmpMinDen = 0;
 
 			// compute the interval boundary for the infixes ending
 			// at the separation point this will be the upper
 			// boundary of the interval
-			numa = 0;
-			numb = 0;
-			tmpMinNum = 0;
-			tmpMinDen = 0;
 			for (int prefix = seppt - 1; prefix >= 0; --prefix) {
 				if (word.charAt(prefix) == 'a') {
 					++numa;
@@ -148,8 +135,8 @@ public class LetterCountingUBM implements UnsolvableBinwordMatcher {
 			// this will be the lower boundary of the interval
 			numa = 0;
 			numb = 0;
-			tmpMaxNum = 0;
-			tmpMaxDen = 0;
+			int tmpMaxNum = 0;
+			int tmpMaxDen = 0;
 			for (int postfix = seppt; postfix < len; ++postfix) {
 				if (word.charAt(postfix) == 'a') {
 					++numa;
@@ -174,22 +161,13 @@ public class LetterCountingUBM implements UnsolvableBinwordMatcher {
 			}
 
 			// save the interval
-			intervals[seppt].set(tmpMinNum, tmpMinDen, tmpMaxNum, tmpMaxDen);
-		}
-
-		// write down which intervals have been handled
-		// 0=not yet handled, 1=in process, 2=already done
-		char[] check = new char[len];
-		for (seppt = 0; seppt < len; ++seppt) {
-			check[seppt] = 0;
+			intervals[seppt] = new Interval(tmpMinNum, tmpMinDen, tmpMaxNum, tmpMaxDen);
 		}
 
 		// check for separation failures
-		for (seppt = 1; seppt < len; ++seppt) {
-			sepLetter = word.charAt(seppt);
+		for (int seppt = 1; seppt < len; ++seppt) {
 			if (!intervals[seppt].isInfinite() && !intervals[seppt].isUnlimited()
 					&& intervals[seppt].leftQuot() <= intervals[seppt].rightQuot()) {
-				check[seppt] = 2;
 				return true;
 			}
 		}
